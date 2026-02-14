42fb7e1ac1559cd5938c3b2f6a53b30d8ae6976b
Diya Santra <santradiya13@gmail.com>
2026-02-14 20:32:26 +0530

    bekar

---
+++ database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+URL_DATABASE = 
+engine = create_engine(
+    URL_DATABASE,
+    pool_pre_ping=True,
+)
+
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
